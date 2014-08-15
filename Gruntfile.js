46fe0d4579eadd4d25975d842fc3c9e79f07bbf7
Jônatas Paganini <jonatasdp@gmail.com>
2014-08-15 00:03:49 -0300

    adding deploy proccess. adding analytics key

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,35 @@ module.exports = function (grunt) {
         }
       }
     },
+    rsync: {
+      options: {
+        args: ["--verbose"],
+        exclude: [".git*","*.scss","node_modules"],
+        recursive: true
+      },
+      dist: {
+        options: {
+          src: '<%= yeoman.dist %>',
+          dest: "../dist"
+        }
+      },
+      stage: {
+        options: {
+          src: "../dist/",
+          dest: "/var/www/sites/stage.bio.ideia.me",
+          host: "jonatas@ideia.me",
+          //delete: true // Careful this option could cause data loss, read the docs!
+        }
+      },
+      prod: {
+        options: {
+          src: "../dist/",
+          dest: "/var/www/sites/bio.ideia.me",
+          host: "jonatas@ideia.me",
+          //delete: true // Careful this option could cause data loss, read the docs!
+        }
+      }
+    },
 
     // Project settings
     yeoman: appConfig,
@@ -482,7 +511,9 @@ module.exports = function (grunt) {
   ]);
 
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.registerTask('deploy', [ 'sshexec:deploy' ]);
+  //grunt.registerTask('deploy', [ 'sshexec:deploy' ]);
   grunt.loadNpmTasks('grunt-ssh');
   grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('deploy', ['build', 'rsync:prod']);
 };
